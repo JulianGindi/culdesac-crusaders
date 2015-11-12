0757559c9fd14b549e765fd31aa4eb7dde9a4283
Julian Gindi <juliangindi@gmail.com>
2015-11-11 22:50:58 -0500

    adding some initial movement scripts

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+
+	public float movementSpeed;
+
+	float horizontalInput;
+	float verticalInput;
+
+	Rigidbody playerRB;
+
+	// Use this for initialization
+	void Start () {
+		playerRB = GetComponent<Rigidbody> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		horizontalInput = Input.GetAxis ("Horizontal");
+		verticalInput = Input.GetAxis ("Vertical");
+
+		print (horizontalInput);
+	}
+}
