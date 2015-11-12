4c3ec9911fa29d3325ce1cb56a24c61e92171aba
Julian Gindi <juliangindi@gmail.com>
2015-11-12 00:05:14 -0500

    adding 'master' directory for a more stable shared testbed

---
+++ Assets/Julian/Scripts/InventoryManager.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InventoryManager : MonoBehaviour {
+
+	public int inventorySize;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
