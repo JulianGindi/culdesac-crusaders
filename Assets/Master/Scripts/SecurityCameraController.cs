b25f3c8e0c93abf46df790392bed858b9a60cbf4
Julian Gindi <juliangindi@gmail.com>
2015-11-23 10:05:40 -0500

    adding security camera stuff

---
+++ Assets/Master/Scripts/SecurityCameraController.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SecurityCameraController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
