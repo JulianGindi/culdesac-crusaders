7a49f35f927f7c1eb57a6a59586331deb2b4796c
Rodrigo Thauby <rthauby@gmail.com>
2015-11-23 11:07:02 -0500

    work on speech

---
+++ Assets/Master/Scripts/UI/Speech.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Speech : MonoBehaviour {
+
+    public GameObject speechCanvas;
+    private InputUtils inputUtils;
+
+	// Use this for initialization
+	void Start () {
+        inputUtils = gameObject.AddComponent<InputUtils>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        inputUtils.AxisToActionEvent("Submit", RespondToButtonPress, RespondToButtonRelease);
+    }
+
+    void RespondToButtonPress() {
+        print("RespondToButtonPress");
+    }
+
+    void RespondToButtonRelease() {
+        print("RespondToButtonRelease");
+    }
+}
