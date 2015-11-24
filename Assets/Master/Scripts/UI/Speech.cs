1de99a9020a378b69aed8c3ad5b8adbcedf0cea2
Rodrigo Thauby <rthauby@gmail.com>
2015-11-23 21:54:06 -0500

    work on speech.

--- Assets/Master/Scripts/UI/Speech.cs
+++ Assets/Master/Scripts/UI/Speech.cs
@@ -1,13 +1,21 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class Speech : MonoBehaviour {
 
     public GameObject speechCanvas;
-    private InputUtils inputUtils;
+    public Transform SpeechBubble;
 
-	// Use this for initialization
-	void Start () {
+    private InputUtils inputUtils;
+    private const int WIDTH = 640;
+    private const int VERTICAL_PADDING = 64;
+    private const int VERTICAL_OFFSET = 64;
+    private const int CHARS_PER_LINE = 32;
+    private const int HEIGHT_PER_LINE = 32;
+
+    // Use this for initialization
+    void Start () {
         inputUtils = gameObject.AddComponent<InputUtils>();
 	}
 	
@@ -17,10 +25,29 @@ public class Speech : MonoBehaviour {
     }
 
     void RespondToButtonPress() {
-        print("RespondToButtonPress");
+        StartCoroutine(Speak());
+    }
+
+    IEnumerator Speak() {
+        string phrases = System.IO.File.ReadAllText("Assets/Text/sample_speech.txt");
+        string[] phrasesArr = phrases.Split('\n');
+        string phrase = phrasesArr[Random.Range(0, phrasesArr.Length - 1)];
+        int numberOfLines = phrase.Length / CHARS_PER_LINE;
+
+        Vector2 positioning = Camera.main.WorldToScreenPoint(transform.position);
+        Transform newElement = Instantiate(SpeechBubble);
+        newElement.transform.Find("Speech").GetComponent<Text>().text = phrase;
+
+        RectTransform rt = newElement.GetComponent<RectTransform>();
+        rt.sizeDelta = new Vector2(WIDTH, (numberOfLines * HEIGHT_PER_LINE) + VERTICAL_PADDING);
+        rt.anchoredPosition = new Vector2(positioning.x, positioning.y + VERTICAL_OFFSET);
+
+        newElement.SetParent(speechCanvas.transform);
+        yield return new WaitForSeconds(3);
+        Destroy(newElement.gameObject);
     }
 
     void RespondToButtonRelease() {
-        print("RespondToButtonRelease");
+        //print("RespondToButtonRelease");
     }
 }
