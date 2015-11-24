90a0691f8f60b224023ea592a8f9299dadbea74b
Julian Gindi <juliangindi@gmail.com>
2015-11-24 10:18:41 -0500

    small tweeks to ai vision

--- Assets/Master/Scripts/AIVision.cs
+++ Assets/Master/Scripts/AIVision.cs
@@ -2,19 +2,10 @@
 using System.Collections;
 
 public class AIVision : MonoBehaviour {
-
-	public float fieldOfViewAngle = 110f;
-
-	// This is a variable for testing purposes. Should be removed before shipping the game.
-	// Has AI follow player character once they enter the "vision collider" instead of actually using realistic vision
-	public bool dumbSight = false;
-
+	
 	[HideInInspector] // We don't want to see this in the editor...just want to be able to access it
 	public bool playerInSight;
 
-	[HideInInspector] // Same
-	public Vector3 personalLastSighting;
-	
 
 	void Start() {
 		playerInSight = false;
@@ -24,14 +15,8 @@ public class AIVision : MonoBehaviour {
 	void OnTriggerStay(Collider other) {
 
 		if (other.CompareTag("Player")) {
-
-			// Create a vector from the enemy to the player and store the angle between it and forward.
-			Vector3 direction = other.transform.position - transform.position;
-			float angle = Vector3.Angle(direction, transform.forward);
-		
-			// If the angle between forward and where the player is, is less than half the angle of view...
-			if(angle < fieldOfViewAngle * 0.5f || dumbSight == true)
-				playerInSight = true;
+			print ("Camera saw player");
+			playerInSight = true;
 		}
 	}
 
--- Assets/Master/Scripts/SecurityCameraController.cs
+++ Assets/Master/Scripts/SecurityCameraController.cs
@@ -2,14 +2,35 @@
 using System.Collections;
 
 public class SecurityCameraController : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
 	
+	public float fieldOfViewAngle = 110f;
+	
+	[HideInInspector] // We don't want to see this in the editor...just want to be able to access it
+	public bool playerInSight;
+
+
+	void Start() {
+		playerInSight = false;
 	}
 	
-	// Update is called once per frame
-	void Update () {
 	
+	void OnTriggerStay(Collider other) {
+		
+		if (other.CompareTag("Player")) {
+			
+			// Create a vector from the enemy to the player and store the angle between it and forward.
+			Vector3 direction = other.transform.position - transform.position;
+			float angle = Vector3.Angle(direction, transform.forward);
+			
+			// If the angle between forward and where the player is, is less than half the angle of view...
+			if(angle < fieldOfViewAngle * 0.5f)
+				print ("Seen by security camera");
+				playerInSight = true;
+		}
+	}
+	
+	void OnTriggerExit(Collider other) {
+		if(other.CompareTag("Player"))
+			playerInSight = false;
 	}
 }
