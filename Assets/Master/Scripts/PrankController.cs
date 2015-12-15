6a4fedc36c13cb0f85abd87626d131f5c65b0591
Julian Gindi <juliangindi@gmail.com>
2015-12-15 12:25:00 -0500

    working on new and improved inventory controller

--- Assets/Master/Scripts/PrankController.cs
+++ Assets/Master/Scripts/PrankController.cs
@@ -3,15 +3,44 @@ using System.Collections;
 
 public class PrankController : MonoBehaviour {
 
-	public class Prank {
+	public static PrankController instance = null;
+	public GameObject personObject;
 
+	// A public list of currently-active Pranks
+	public Prank[] activePranks;
+
+	public class Prank: MonoBehaviour {
 		public string name;
-		public string type; // Projectile, 'bomb', aoe, graffitti...
 
-		public Prank(string prankName, string prankType) {
-			name = prankName;
-			type = prankType;
+		public Prank(string name) {
+			name = name;
 		}
+	}
+	
+
+	public interface IPrankable {
+		// This is a function that MUST be implemented by any prank class
+		void Trigger();
+	}
+
+	public class FireCracker: Prank, IPrankable {
+		public void Trigger() {
+			print ("Prank triggering");
+		}
+	}
+	
+
+	// Boilerplate static GameManager stuff
+	void Awake () {
+		if (instance == null)
+			instance = this;
+		else if (instance != this)
+			Destroy (gameObject);
+		
+		DontDestroyOnLoad (gameObject);
+	}
+	
+	void Start() {
 
 	}
 }
