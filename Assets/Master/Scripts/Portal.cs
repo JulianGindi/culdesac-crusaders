814ceed4fd87cbd00a5453903c77fa21cf702ff7
Rodrigo Thauby <rthauby@gmail.com>
2015-12-22 00:40:12 -0500

    first pass at doorways

---
+++ Assets/Master/Scripts/Portal.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Portal : MonoBehaviour {
+
+    public GameObject exitPortal;
+
+    Vector3 exitPoint;
+
+    void Awake() {
+        exitPoint = transform.Find("Exit Point").transform.position;
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        other.SendMessage("CrossedPortalTo", exitPortal.GetComponent<Portal>());
+    }
+
+    public Vector3 GetExitPoint() {
+        return exitPoint;
+    }
+
+}
