d16da7095dc8ac1a7e852c9b783ba21a144f2b17
Julian Gindi <juliangindi@gmail.com>
2015-12-22 00:45:48 -0500

    working on player attack

--- Assets/AI/Actions/AttackPlayer.cs
+++ Assets/AI/Actions/AttackPlayer.cs
@@ -1,12 +1,18 @@
 using UnityEngine;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using RAIN.Action;
 using RAIN.Core;
+using RAIN.Representation;
+using RAIN.Navigation;
 
 [RAINAction]
 public class AttackPlayer : RAINAction
 {
+
+	public Expression TotalAmmo = new Expression();
+
     public override void Start(RAIN.Core.AI ai)
     {
         base.Start(ai);
@@ -14,6 +20,7 @@ public class AttackPlayer : RAINAction
 
     public override ActionResult Execute(RAIN.Core.AI ai)
     {
+		// First we will retrieve the player we want to attack
         return ActionResult.SUCCESS;
     }
 
