315fda4ce1bb46a2ad74664d21a915325a89430f
Julian Gindi <juliangindi@gmail.com>
2015-12-31 13:14:14 -0500

    implemented basic player damage. Player slows down when hit

---
+++ Assets/Master/Scripts/Player/PlayerDamage.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerDamage : MonoBehaviour {
+
+	public float Hitpoints = 20.0f; // Arbitrary default for now
+	public float MovementReduction;
+	public bool Grounded = false;
+
+	PlayerMovement movement;
+
+	void Start() {
+		movement = gameObject.GetComponent<PlayerMovement>();
+	}
+
+	void OnCollisionEnter(Collision collision) {
+		if (collision.gameObject.CompareTag("Projectile")) {
+			float newSpeed = movement.walkSpeed - MovementReduction;
+			if (newSpeed <= 0) {
+				Grounded = true;
+				newSpeed = 0;
+			}
+			movement.walkSpeed = newSpeed;
+			movement.runSpeed = newSpeed + newSpeed * 1.75f;
+		}
+	}	
+}
