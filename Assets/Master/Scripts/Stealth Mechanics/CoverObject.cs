59084b1fd0654a33f76c22775648dac00861f089
Julian Gindi <juliangindi@gmail.com>
2016-01-25 01:05:02 -0500

    figured out the cover objects real purpose in life

--- Assets/Master/Scripts/Stealth Mechanics/CoverObject.cs
+++ Assets/Master/Scripts/Stealth Mechanics/CoverObject.cs
@@ -11,37 +11,33 @@ public class CoverObject : MonoBehaviour {
 	public bool isCovering = false; // Is this object currently covering any player?
 
 	List<GameObject> playersCovered; // Reference where we will store the players being covered
-	bool isIndicating; // Don't keep indicating if already indicating
 
-	List<GameObject> playersInRange; // Players who can seek cover but have not yet
 
 	void Start() {
-		isIndicating = false;
-
 		playersCovered = new List<GameObject>();
-		playersInRange = new List<GameObject>();
-
 	}
 
-	void Update() {
-		if ((playersInRange.Count > 0) && canCoverAnotherPlayer() && !isIndicating) {
-			displayCoverIndicator();
-			isIndicating = true;
-		}
+	bool canCoverAnotherPlayer() {
+		return (playersCovered.Count < coverCount);
 	}
 
-	void OnTriggerEnter(Collider other) {
-		if (other.CompareTag("Player"))
-			playersInRange.Add(other.gameObject);
-	}
-
-	void displayCoverIndicator() {
+	public void DisplayCoverIndicator() {
 		// Instantiate cover indicator behind object
 		Transform spawnLocation; // Middle of object and just slightly behind
 		Instantiate(coverIndicator, indicatorPosition.position, Quaternion.identity);
 	}
-		
-	bool canCoverAnotherPlayer() {
-		return (playersCovered.Count < coverCount);
+
+	public void ClearCoveredPlayers() {
+		playersCovered.Clear();
+	}
+
+	// This function attempts to  add a player to cover.
+	// If the "cover is full" the function will return false
+	public bool AddPlayerToCover(GameObject player) {
+		if (canCoverAnotherPlayer()) {
+			playersCovered.Add(player);
+			return true;
+		}
+		return false;
 	}
 }
